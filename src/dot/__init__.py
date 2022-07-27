98948f17e2a6a13db36cff2938ba5f28a0c345f1
giorgiop <giorgiop@users.noreply.github.com>
2022-07-27 13:02:13 +0200

    Bump version: 1.0.0 → 1.1.0

--- src/dot/__init__.py
+++ src/dot/__init__.py
@@ -6,7 +6,7 @@ licensed under the BSD 3-Clause "New" or "Revised" License.
 
 from .dot import DOT
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __author__ = "Sensity"
 __url__ = "https://github.com/sensity-ai/dot/tree/main/dot"
 __docs__ = "Deepfake offensive toolkit"
